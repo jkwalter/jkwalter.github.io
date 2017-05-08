cf7816510c9786da90b8823f372c3846780863e1
jkwalter <joshwalter2012@outlook.com>
2017-05-08 18:26:24 -0500

    Initial commit

---
+++ C/fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+ int i;
+ int num;
+ long x;
+ long y;
+
+ x = 0;
+ y = 1;
+
+ printf("===================\n");
+ printf(" FIBONACCI NUMBERS \n");
+ printf("===================\n");
+ do
+  {
+   printf("Enter a number greater than 1 and less than 92: ");
+   scanf("%d", &num);
+  } while (num <= 1 && num > 92);
+
+ printf("\n");
+
+ printf(" 0: %ld\n", x);
+ printf(" 1: %ld\n", y);
+
+ for (i = 2; i <= num; i++)
+  {
+   y = x + y;
+   x = y - x;
+   printf("%2d: %ld\n", i, y);
+  }
+
+ return 0;
+}

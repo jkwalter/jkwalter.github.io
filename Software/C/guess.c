f9c67bfb8f64641eae0e8b6f4b024f1eef1fdb00
jkwalter <joshwalter2012@outlook.com>
2017-05-09 15:38:11 -0500

    Initial commit

---
+++ Software/C/guess.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+ int num;
+ int guess;
+ int used[10] = {0};
+
+ srand(time(NULL));
+
+ num = rand() % 9 + 1;
+
+ printf("------------------------\n");
+ printf("     GUESSING GAME!\n");
+ printf("------------------------\n");
+
+ do
+  {
+   printf("Enter a number between 1 and 9: ");
+   scanf("%d", &guess);
+   if (guess < 1)
+    {
+     printf("%d is smaller than 1.\n", guess);
+     printf("\n");
+    }
+   else if (guess > 9)
+    {
+     printf("%d is larger than 9.\n", guess);
+     printf("\n");
+    }
+   else if (guess < num)
+    {
+     if (used[guess] == 1)
+      {
+       printf("%d has already been guessed!\n", guess);
+       printf("\n");
+      }
+     else
+      {
+       printf("%d is too small!\n", guess);
+       printf("\n");
+       used[guess] = 1;
+      }
+    }
+   else if (guess > num)
+    {
+     if (used[guess] == 1)
+      {
+       printf("%d has already been guessed!\n", guess);
+       printf("\n");
+      }
+     else
+      {
+       printf("%d is too large!\n", guess);
+       printf("\n");
+       used[guess] = 1;
+      }
+    }
+  } while (guess != num);
+
+ printf("CONGRATULATIONS! %d is the correct number!\n", guess);
+
+ return 0;
+}

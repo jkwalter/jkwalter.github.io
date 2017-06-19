04241213af3b6c9738d965e26349fd14520597dd
jkwalter <joshwalter2012@outlook.com>
2017-06-19 17:26:13 -0500

    Initial commit

--- Software/C++/hello.cpp
+++ Software/C++/hello.cpp
@@ -3,11 +3,23 @@ Compiling and Running in Linux:
 
  - Open a terminal
  - Navigate to the same directory as hello.cpp
+      cd ~/Programs/jkwalter.github.io/Software/C++
  - Type the following to complie hello.cpp
       g++ hello.cpp
  - Type the following to run hello.cpp
       ./a.out
+
+Compiling and Running in Microsoft Visual Studio:
+
+ - Open Developer Command Prompt for Visual Studio
+ - Navigate to the same directory as hello.cpp
+      cd C:\Users\Josh\Documents\Programs\C++
+ - Type the following to compile hello.cpp
+      cl hello.cpp
+ - Type the following to run hello.cpp
+      hello.exe
 */
+
 #include <iostream>
 
 using namespace std;
--- Software/C/hello.c
+++ Software/C/hello.c
@@ -13,7 +13,7 @@ Compiling and Running in Microsoft Visual Studio:
 
  - Open Developer Command Prompt for Visual Studio
  - Navigate to the same directory as hello.c
-      cd C:\Users\Josh\Downloads
+      cd C:\Users\Josh\Documents\Programs\C
  - Type the following to compile hello.c
       cl hello.c
  - Type the following to run hello.c

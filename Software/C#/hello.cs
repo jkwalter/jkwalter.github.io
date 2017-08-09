9d29398674b5ac85f2510a45105fe74c8ad46d36
Josh Walter <joshwalter2012@outlook.com>
2017-08-09 13:41:13 -0500

    Initial commit

---
+++ Software/C#/hello.cs
@@ -0,0 +1,19 @@
+﻿/*
+Compiling and Running in Microsoft Visual Studio:
+
+ - Open Developer Command Prompt for Visual Studio
+ - Navigate to the same directory as hello.cs
+      cd "C:\Users\Josh Walter\Documents\Visual Studio 2017\Projects"
+ - Type the following to compile hello.cs
+      csc hello.cs
+ - Type the following to run hello.cs
+      hello
+*/
+
+public class hello
+{
+	public static void Main()
+	{
+       System.Console.WriteLine("Hello world!");
+	}
+}

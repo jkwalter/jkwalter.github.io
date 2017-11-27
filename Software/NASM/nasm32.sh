d9310561d46ded5089ddd219af31cbf86bb46b83
jkwalter <joshwalter2012@outlook.com>
2017-11-27 12:58:08 -0600

    Initial commit

--- Software/NASM/nasm32.sh
+++ Software/NASM/nasm32.sh
@@ -3,7 +3,5 @@
 # INSTALL PACKAGES TO RUN 32-BIT
 # NASM PROGRAMS ON 64-BIT LINUX
 # ==============================
-sudo dpkg --add-architecture i386
-sudo apt-get update
 sudo apt-get install nasm
 sudo apt-get install gcc-multilib

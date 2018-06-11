3fea9646826c488655d0c4545a78b6394f86db40
Josh Walter <joshwalter2012@outlook.com>
2018-06-11 15:27:25 -0500

    Initial commit

--- Software/C#/battle.cs
+++ Software/C#/battle.cs
@@ -2,12 +2,12 @@
 Compiling and Running in Microsoft Visual Studio:
 
  - Open Developer Command Prompt for Visual Studio
- - Navigate to the same directory as battle.cs
-      cd "C:\Users\Josh Walter\Documents\Programs\C#"
- - Type the following to compile battle.cs
-      csc battle.cs
- - Type the following to run battle.cs
-      battle
+ - Navigate to the same directory as Battle.cs
+      cd "C:\Users\Josh Walter\source\repos\jkwalter.github.io\Software\C#"
+ - Type the following to compile Battle.cs
+      csc Battle.cs
+ - Type the following to run Battle.cs
+      Battle
 */
 
 class Character
--- Software/C#/hello.cs
+++ Software/C#/hello.cs
@@ -2,18 +2,20 @@
 Compiling and Running in Microsoft Visual Studio:
 
  - Open Developer Command Prompt for Visual Studio
- - Navigate to the same directory as hello.cs
-      cd "C:\Users\Josh Walter\Documents\Programs\C#"
- - Type the following to compile hello.cs
-      csc hello.cs
- - Type the following to run hello.cs
-      hello
+ - Navigate to the same directory as Hello.cs
+      cd "C:\Users\Josh Walter\source\repos\jkwalter.github.io\Software\C#"
+ - Type the following to compile Hello.cs
+      csc Hello.cs
+ - Type the following to run Hello.cs
+      Hello
 */
 
-public class hello
+using System;
+
+public class Hello
 {
 	public static void Main()
 	{
-		System.Console.WriteLine("Hello world!");
+        Console.WriteLine("Hello world!");
 	}
 }

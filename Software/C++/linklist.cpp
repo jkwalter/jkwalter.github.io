054eeab293affed9c7465c1b53a4e7453ccdd7b0
jkwalter <joshwalter2012@outlook.com>
2018-10-22 17:28:47 -0500

    Initial commit

--- Software/C++/linklist.cpp
+++ Software/C++/linklist.cpp
@@ -3,7 +3,7 @@ Compiling and Running in UNIX:
 
  - Open a terminal
  - Navigate to the same directory as linklist.cpp
-      cd ~/Documents/Programs/jkwalter.github.io/Software/C
+      cd ~/Documents/Programs/jkwalter.github.io/Software/C++
  - Type the following to compile linklist.cpp
       gcc linklist.cpp
  - Type the following to run linklist.cpp
@@ -13,7 +13,7 @@ Compiling and Running in Microsoft Visual Studio:
 
  - Open Developer Command Prompt for Visual Studio
  - Navigate to the same directory as linklist.cpp
-      cd "C:\Users\Josh Walter\Documents\Programs\C"
+      cd "C:\Users\Josh Walter\Documents\Programs\C++"
  - Type the following to compile linklist.cpp
       cl linklist.cpp
  - Type the following to run linklist.cpp

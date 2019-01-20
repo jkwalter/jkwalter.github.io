7356f29f13dc73451ac85b44437ad888dec90fcf
jkwalter <joshuakwalter@outlook.com>
2019-01-19 21:56:38 -0600

    Initial commit

--- Software/NASM/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ================================
-# Install nasm and gcc-multilib
-# To Assemble 32-bit NASM Programs
-# On 64-bit Ubuntu
-# ================================
-sudo apt-get install nasm
-sudo apt-get install gcc-multilib
